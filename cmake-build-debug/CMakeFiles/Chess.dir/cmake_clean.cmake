a523b211f1bccc9420ca42bfb6f3f5d8de7ff36e
Raymond jones <Rayjones2170@gmail.com>
2018-05-16 14:58:49 -0400

    Adds Clion files

---
+++ cmake-build-debug/CMakeFiles/Chess.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chess.dir/main.cpp.o"
+  "Chess.pdb"
+  "Chess.exe"
+  "libChess.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chess.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
